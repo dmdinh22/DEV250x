011852e114fb890a522b171ae7e65ced39e5a64e
David Dinh <dmdinh22@gmail.com>
2018-05-03 16:01:20 -0700

    Module 2 Lab 1

---
+++ MyApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyApp.Models;
+using System.Linq;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new sakilaContext();
+            var actors = dbContext.Actor.ToList();
+            foreach (var a in actors) {
+                System.Console.WriteLine($"ID:{a.ActorId} Name:{a.FirstName} {a.LastName}");
+            }
+        }
+    }
+}
