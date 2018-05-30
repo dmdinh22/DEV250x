79957263365f9a9ab4137242a61b15058f24620f
David Dinh <dmdinh22@gmail.com>
2018-05-30 16:19:52 -0700

    edit action

--- ProductManagementApp/Controllers/ProductTypeController.cs
+++ ProductManagementApp/Controllers/ProductTypeController.cs
@@ -9,5 +9,38 @@ namespace ProductManager.Controllers {
         }
 
         // other actions
+        [HttpGet]
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductType productType) {
+            if (ModelState.IsValid) {
+                DataSource.AddProductType(productType);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) {
+            var pt = DataSource.GetProductTypeByID(id);
+            return View(pt);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductType productType) {
+            if (ModelState.IsValid) {
+                DataSource.UpdateProductTypeByID(productType.ID, productType);
+            }
+
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Delete(int id) {
+            DataSource.RemoveProductTypeByID(id);
+            return RedirectToAction("Index");
+        }
     }
 }
